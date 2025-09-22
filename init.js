36052df7dde07c217e7da6766183b275ca8be1dd
anonymous200225 <mardhi.project@gmail.com>
2025-09-22 18:59:31 +0700

    Create init.js

---
+++ init.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("demo-cache").then(c => 
+      c.addAll([
+        "/",
+        "/index.html"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
