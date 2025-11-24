dc2d74825f40c62837ab39b52b322153b20920d0
anonymous200225 <mardhi.project@gmail.com>
2025-11-24 19:58:00 +0700

    Update init.js

--- init.js
+++ init.js
@@ -1,32 +1,11 @@
-/*
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.map(key => {
-          if (key !== "demo-cache") {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister());
+  });
+}
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("demo-cache").then(c => 
-      c.addAll([
-        "/",
-        "/index.html"
-      ])
-    )
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
-*/
+if ('caches' in window) {
+  caches.keys().then(keys => {
+    keys.forEach(key => caches.delete(key));
+  });
+}
