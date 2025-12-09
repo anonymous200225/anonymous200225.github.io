78dfac5847e10222836d6de64b12ea587a833869
anonymous200225 <mardhi.project@gmail.com>
2025-12-09 22:28:07 +0700

    Update sw.js

--- web-ide/sw.js
+++ web-ide/sw.js
@@ -1,10 +1,23 @@
 const CACHE_NAME = "web-ide-v1";
+const BASE = "/web-ide";
+
 const FILES = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  `${BASE}/`,
+  `${BASE}/index.html`,
+  `${BASE}/manifest.json`,
+  `${BASE}/icon-192.png`,
+  `${BASE}/icon-512.png`,
+  `${BASE}/css/app.css`,
+  `${BASE}/js/app.js`,
+  `${BASE}/css/modal.css`,
+  `${BASE}/css/all.min.css`,
+  `${BASE}/js/beautify.min.js`,
+  `${BASE}/js/beautify-css.min.js`,
+  `${BASE}/js/beautify-html.min.js`,
+  `${BASE}/webfonts/fa-brands-400.woff2`,
+  `${BASE}/webfonts/fa-regular-400.woff2`,
+  `${BASE}/webfonts/fa-solid-900.woff2`,
+  `${BASE}/webfonts/fa-v4compatibility.woff2`
 ];
 
 self.addEventListener("install", e => {
