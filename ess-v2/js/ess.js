fd02267747b9a9892c5393c80f1ecd3136536c4e
anonymous200225 <mardhi.project@gmail.com>
2026-09-13 17:52:31 +0700

    Update Google Script URL in ess.js

--- ess-v2/js/ess.js
+++ ess-v2/js/ess.js
@@ -10,7 +10,7 @@ const JP = GAJI_POKOK * 0.01;
 const POTONGAN = JHT + JP;
 
 /* GANTI dengan URL Web App Apps Script hasil deployment (/exec). */
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzYNnxaOnDszmY_Q2N4MbB5lsvsv9X1YaRjFkVFCUUs4lwsOGtp5Eq0yvmapFvflPuZ/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwESrs-vOHhOz0Pglz8uVpLAoRoaaWTLL-Womin4gvwvLUX_9DBSMGoKLP-eEx9DrkX2A/exec";
 const AUTO_SYNC_MS = 60 * 1000;
 const LS_QUEUE = "essPendingQueue";
 const LS_LAST_SYNC = "essLastSync";
